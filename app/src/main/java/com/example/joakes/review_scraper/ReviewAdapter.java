43fc4cc0c8d1578b8218d69c4e3cc889b35fb770
Jacob Oakes <jacoboakes92@gmail.com>
2014-09-08 21:33:05 -0400

    refactoring

--- app/src/main/java/com/example/joakes/review_scraper/ReviewAdapter.java
+++ app/src/main/java/com/example/joakes/review_scraper/ReviewAdapter.java
@@ -15,6 +15,7 @@ import android.widget.TextView;
 public class ReviewAdapter extends ArrayAdapter<Review> {
     private final Context context;
     private final Review[] values;
+    private View rowView;
 
     public ReviewAdapter(Context context, Review[] values) {
         super(context, R.layout.review_list_item3, values);
@@ -27,24 +28,18 @@ public class ReviewAdapter extends ArrayAdapter<Review> {
         LayoutInflater inflater = (LayoutInflater) context
                 .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         Review review = values[position];
-        View rowView = inflater.inflate(R.layout.review_list_item3, parent, false);
-        handleTextViewWithDefault(rowView, R.id.review_site, review.site, context.getString(R.string.default_site));
-        handleTextViewWithDefault(rowView, R.id.rating_value, review.rating, context.getString(R.string.default_rating));
-        handleTextViewWithDefault(rowView, R.id.community_rating_value, review.communityRating, context.getString(R.string.default_rating));
-        handleTextViewPair(
-                rowView,
-                R.id.rating_description,
-                R.id.community_rating_description,
-                review.ratingDescription,
-                review.communityRatingDescription
-        );
-        handleLink(rowView, R.id.review_link, review.reviewLink, context.getString(R.string.review_link_description));
-        handleLink(rowView, R.id.video_review_link, review.videoReviewLink, context.getString(R.string.video_link_description));
+        rowView = inflater.inflate(R.layout.review_list_item3, parent, false);
+        handleTextViewWithDefault(R.id.review_site, review.site, context.getString(R.string.default_site));
+        handleTextViewWithDefault(R.id.rating_value, review.rating, context.getString(R.string.default_rating));
+        handleTextViewWithDefault(R.id.community_rating_value, review.communityRating, context.getString(R.string.default_rating));
+        handleTextViewPair(R.id.rating_description, R.id.community_rating_description, review.ratingDescription, review.communityRatingDescription);
+        handleLink(R.id.review_link, review.reviewLink, context.getString(R.string.review_link_description));
+        handleLink(R.id.video_review_link, review.videoReviewLink, context.getString(R.string.video_link_description));
         return rowView;
     }
 
-    private void handleTextViewWithDefault(View parentView, int viewId, String text, String defaultText){
-        TextView view = (TextView) parentView.findViewById(viewId);
+    private void handleTextViewWithDefault(int viewId, String text, String defaultText){
+        TextView view = (TextView) rowView.findViewById(viewId);
         if(text != null){
             view.setText(text);
         } else{
@@ -52,9 +47,9 @@ public class ReviewAdapter extends ArrayAdapter<Review> {
         }
     }
 
-    private void handleTextViewPair(View parentView, int viewId1, int viewId2, String text1, String text2){
-        TextView view1 = (TextView) parentView.findViewById(viewId1);
-        TextView view2 = (TextView) parentView.findViewById(viewId2);
+    private void handleTextViewPair(int viewId1, int viewId2, String text1, String text2){
+        TextView view1 = (TextView) rowView.findViewById(viewId1);
+        TextView view2 = (TextView) rowView.findViewById(viewId2);
         if (text1 == null && text2 == null){
             view1.setVisibility(View.GONE);
             view2.setVisibility(View.GONE);
@@ -70,8 +65,9 @@ public class ReviewAdapter extends ArrayAdapter<Review> {
         }
     }
 
-    private void handleLink(View parentView, int viewId, String url, String linkDescriptor){
-        TextView view = (TextView) parentView.findViewById(viewId);
+    // could use handleTextViewPair
+    private void handleLink(int viewId, String url, String linkDescriptor){
+        TextView view = (TextView) rowView.findViewById(viewId);
         if (url == null){
             view.setVisibility(View.GONE);
         } else {
